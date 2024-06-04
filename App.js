075419bd3f66e69859ea91bde1ffd6b4871dc0eb
pandaman <fanda.alvaridzi01@gmail.com>
2024-06-04 13:57:37 +0700

    init

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { View, StyleSheet } from 'react-native'
+import WeatherSearch from './src/components/WeatherSearch'
+import WeatherInfo from './src/components/WeatherInfo'
+import axios from 'axios'
+import { BASE_URL,API_KEY } from './src/helper/constant'
 
-export default function App() {
+const App = () => {
+  const [weatherData,setWeatherData]=useState()
+  const searchWeather =(location)=>{
+    console.log(location)
+    axios.get(`${BASE_URL}?q=${location},id&APPID=${API_KEY}`).then((res)=>{
+      console.log(res)
+      const data = res.data
+
+      data.visibility /=1000
+      data.visibility = data.visibility.toFixed(2)
+      data.main.temp -=273.15
+
+      //Konversi Kelvin ke Celcius
+      data.main.temp = data.main.temp.toFixed(2)
+      setWeatherData(data)
+
+      console.log(data)
+    }).catch((err)=>{
+      console.log(err)
+    })
+    // console.log("hai")
+  } 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WeatherSearch searchWeather={searchWeather} />
+      {weatherData && <WeatherInfo weatherData={weatherData} />}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
   },
-});
+})
+
+export default App
---
+++ src/helper/constant.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export const API_KEY = 'fa83b00ff5aaf1804706018f0b5e8d05';
